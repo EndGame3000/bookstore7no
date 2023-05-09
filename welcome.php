f179a6625fc84b1c9655ef0fa54af7eabed52109
EndGame3000 <132250299+EndGame3000@users.noreply.github.com>
2023-05-09 08:45:43 +0530

    Add files via upload

---
+++ welcome.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Homepage- Prakhar's Book Store</title>
+</head>
+<body>
+    <h1>Welcome to Prakhar's Book Store</h1>
+    <p>You can purchase books online here</p>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cum a error veritatis laudantium natus corrupti illum numquam, odit nisi consectetur repellat facere magni nesciunt. Nihil, perspiciatis tenetur. Harum, ut et fuga aperiam voluptatum perferendis odio accusamus labore omnis explicabo a laborum officia quae velit neque doloremque eaque aliquid exercitationem quod! Neque harum odio fugiat commodi. Minima, sed eaque! Deleniti omnis explicabo, voluptatibus quae esse unde sapiente beatae in autem, ea architecto accusantium tempora praesentium veniam nesciunt ratione doloribus iste eius error quis quidem id eos doloremque. Error reiciendis fuga fugiat autem saepe totam, nostrum, natus corporis illo nulla voluptate vitae?</p>
+</body>
+</html>
